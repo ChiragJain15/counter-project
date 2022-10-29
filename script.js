e55c47bc39409a0561f11db31a662504369fbe89
ChiragJain15 <chiragjain150302@gmail.com>
2022-10-29 12:43:13 +0530

    updated counter date

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const days = document.getElementById("days");
-const minutes = document.getElementById("minutes");
-const hours = document.getElementById("hours");
-const seconds = document.getElementById("seconds");
-const date = "6 May 2022";
+const days = document.getElementById('days');
+const minutes = document.getElementById('minutes');
+const hours = document.getElementById('hours');
+const seconds = document.getElementById('seconds');
+const date = '15 Mar 2023';
 const goal = new Date(date);
 
 function counter() {
-  const today = new Date();
-  time = goal - today;
-  time = Math.floor(time / 1000);
-  let second = Math.floor(time % 60);
-  let minute = Math.floor((time / 60) % 60);
-  let hour = Math.floor((time / 3600) % 24);
-  let day = Math.floor(time / 3600 / 24);
-  if (second < 10) seconds.innerHTML = `0${second}`;
-  else seconds.innerHTML = second;
-  if (minute < 10) minutes.innerHTML = `0${minute}`;
-  else minutes.innerHTML = minute;
-  if (hour < 10) hours.innerHTML = `0${hour}`;
-  else hours.innerHTML = hour;
-  if (day < 10) days.innerHTML = `0${day}`;
-  else days.innerHTML = day;
+    const today = new Date();
+    time = goal - today;
+    time = Math.floor(time / 1000);
+    let second = Math.floor(time % 60);
+    let minute = Math.floor((time / 60) % 60);
+    let hour = Math.floor((time / 3600) % 24);
+    let day = Math.floor(time / 3600 / 24);
+    if (second < 10) seconds.innerHTML = `0${second}`;
+    else seconds.innerHTML = second;
+    if (minute < 10) minutes.innerHTML = `0${minute}`;
+    else minutes.innerHTML = minute;
+    if (hour < 10) hours.innerHTML = `0${hour}`;
+    else hours.innerHTML = hour;
+    if (day < 10) days.innerHTML = `0${day}`;
+    else days.innerHTML = day;
 }
 
 counter();
